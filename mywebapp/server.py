20fd037b854604353d3e7cb9dd06e99bf2afcad6
Gregory Spain <spaing3850@clarkstate.edu>
2023-04-08 14:02:34 -0400

    added config call

--- mywebapp/server.py
+++ mywebapp/server.py
@@ -3,6 +3,7 @@ import views
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("settings")
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/animals", view_func=views.animals_page)
     return app
@@ -11,5 +12,6 @@ if __name__ == "__main__":
     app = create_app()
     port = app.config.get("PORT", 5000)
     app.run(host="127.0.0.1", port=port)
+    app.config.get
 
 
